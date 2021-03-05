595298505c07cf8d4a9fa0ae107de80ea4da4ab7
Tyler Stinson <tylerastinson08@outlook.com>
2021-03-05 11:57:23 -0500

    This is what I did: added email

--- MultiFile/ClassPerson.cs
+++ MultiFile/ClassPerson.cs
@@ -5,12 +5,14 @@ public class Person
 	private string _name;
 	private string _height;
 	private string _weight;
+	private string _email;
 
-	public Person(string Name, string Height, string Weight)
+    public Person(string Name, string Height, string Weight, string Email)
 	{
 		_name = Name;
 		_height = Height;
 		_weight = Weight;
+		_email = Email;
 	}
 
 	public string Nane
@@ -31,8 +33,13 @@ public class Person
 		set { _weight = value; }
     }
 
+	public string Email
+	{
+		get {return _email; }
+		set {_email = value; }
+	}
 	public void display()
     {
-		Console.WriteLine("Name: {0}    Height: {1}    Weight: {2}", _name, _height, _weight);
+		Console.WriteLine("Name: {0}    Height: {1}    Weight: {2}   Email: {3}", _name, _height, _weight, _email);
     }
 }
