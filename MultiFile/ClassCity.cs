66743d8bcc26b46b6e5570b39b0cbf62b84fd357
omicron hex <omicronhex@outlook.com>
2021-03-05 11:52:30 -0500

    Did class c

--- MultiFile/ClassCity.cs
+++ MultiFile/ClassCity.cs
@@ -4,27 +4,35 @@ public class City
 {
 	private string _cityName;
 	private string _state;
+	private int _population;
 
-	public City(string Name, string State)
+	public City(string Name, string State, int Population)
 	{
 		_cityName = Name;
 		_state = State;
+		_population = Population;
 	}
 
 	public string CityName
-    {
+	{
 		get { return _cityName; }
 		set { _cityName = value; }
-    }
+	}
 
 	public string State
-    {
+	{
 		get { return _state; }
 		set { _state = value; }
-    }
+	}
+
+	public int Population
+	{
+		get { return _population; }
+		set { _population = value; }
+	}
 
 	public void display()
-    {
-		Console.WriteLine("{0},  {1}", _cityName, _state);
-    }
+	{
+		Console.WriteLine("{0},  {1}", _cityName, _state, _population);
+	}
 }
